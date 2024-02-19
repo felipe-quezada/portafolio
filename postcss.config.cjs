a2cd20de7b7037a7ba2a0f25d64229cfe1ab51c8
Luis Quezada <fluis285@gmail.com>
2024-02-19 10:59:24 -0500

    portafolio v1.0

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,7 +1,19 @@
+// const pokeImage = require('./src/')
+
 module.exports = {
 	plugins: [
 		require('autoprefixer'),
 		require('postcss-nested'),
-		require('postcss-mixins'),
+		require('postcss-mixins')({
+			mixins: {
+				images: (mixin, name) => {
+					return {
+						'background-image': `url('src/assets/${name}-desktop.png')`,
+						animation: `${name} 12s infinite`,
+						'animation-play-state': 'paused',
+					};
+				},
+			},
+		}),
 	],
 };
